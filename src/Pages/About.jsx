158424c0f0e1d9ce09c9150d842dec849cdd140d
Dharanikumar K <kdharanstarflit@gmail.com>
2023-03-24 10:19:13 +0530

    Resume

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -20,10 +20,9 @@ const About = () => {
 						</h3>
 						<p className="mb-4">
 							{/* I'm a FullStack Developer. My expertise is to create Web Application and Websites Design. */}
-							Hello, I am DHARANIKUMAR K, MBA & Engineering graduate student. Aspiring Full Stack Developer with a focus
-							on the MERN stack who is passionate and dedicated. I want to use my technical knowledge and skills to
-							build and develop high-quality, user-friendly platforms. I'm looking for a challenging position at a
-							fast-paced company where I can expand my knowledge and contribute to exciting projects.
+							Hello, I am DHARANIKUMAR K, MBA & Engineering graduate student. Aspiring Full Stack Developer with a focus on the MERN stack who is
+							passionate and dedicated. I want to use my technical knowledge and skills to build and develop high-quality, user-friendly platforms.
+							I'm looking for a challenging position at a fast-paced company where I can expand my knowledge and contribute to exciting projects.
 						</p>
 						<ul className="mb-4 p-0">
 							<li>
@@ -55,7 +54,7 @@ const About = () => {
 						{/* <a href={Cv} download="Resume">
 							<button>DOWNLOAD CV</button>
 						</a> */}
-						<a href="https://drive.google.com/file/d/1_EnxN1Wh4wsIP4EigSg-a6DiCdNtQtpO/view?usp=sharing" target="blank">
+						<a href="https://drive.google.com/file/d/1FeeDHfexmDF4-L8ye-CoI9WhPs3z7I9d/view?usp=sharing" target="blank">
 							{/* {Cv} download="Resume" */}
 							<button>VIEW RESUME</button>
 						</a>
